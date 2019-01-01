4747e9b88f1b5961bfa9f12698a00e68ae9e56a1
github username <github email address>
2019-01-01 14:54:55 -0600

    Created MVCs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    
+    def create 
+       User.create(user_params)
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :pictures
+
+
+
+    validates :username, length: {minimum: 4}
+    validates :username, uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 4}
+
+
 end
